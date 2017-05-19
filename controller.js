4c548e288d99efe1b206bcead2b21e8bdacd995d
Veronika <nika.dzhabbarova@gmail.com>
2017-05-19 21:59:55 +0300

    adding controller

---
+++ controller.js
@@ -0,0 +1,24 @@
+
+var app = angular.module('app', []);
+
+var req = {
+ method: 'GET',
+ url: 'http://sanctuary/rest/book',
+ headers: {
+   'Content-Type': 'json'
+ },
+ message: 'Connected',
+ data: { test: 'test' }
+};
+
+app.controller('book', function ($scope, $http){
+    if ($http(req)){
+        alert(req.message);
+    }
+	else {
+		alert "Не подключено!";
+	}
+    $http(req).then (function($response){
+                $scope.test = response;
+            });
+});
